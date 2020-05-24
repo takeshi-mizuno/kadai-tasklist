a453d1be5c48f5ecb307e0a598a7e093ca96a1a5
T.M <takeshi.031.mizuno@gmail.com>
2020-05-24 12:12:18 +0000

    route Fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,10 @@ class TasksController < ApplicationController
 #  before_action :require_user_logged_in, only: [:index, :show]
   before_action :require_user_logged_in
 #  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show,:edit,:update, :destroy,]
+
+#correct:useridをしている処理のため、idを使用していない箇所には必要ない（e.g)index）
+  before_action :correct_user, only: [:show,:edit,:update, :destroy]
+
   
   def index
 #    if logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+#  get 'toppages/index'
 #  get 'users/index'
 #  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+#  get 'users/new'
+#  get 'users/create'
+#  get 'sessions/new'
+#  get 'sessions/create'
+#  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'tasks#index'
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 #  resources :users, only: [:index, :show, :new, :create]
   resources :users, only: [:new, :create]
 
-  resources :tasks, only: [:index, :show, :new, :create, :destroy,:edit,:update]
+#  resources :tasks, only: [:index, :show, :new, :create, :destroy,:edit,:update]
+    resources :tasks
   
 end
